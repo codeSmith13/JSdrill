dbaa010129959c4f33c6fec7a91fccb4b8aef35e
codeSmith13 <peeperz13@gmail.com>
2021-05-10 12:50:05 -0500

    first msg

---
+++ app.js
@@ -0,0 +1,18 @@
+let name= ("chris");
+console.log (name);
+
+const numOfStates =(51);
+
+console.log(numOfStates);
+console.log(sayHello);
+console.log(greeter);
+greeter=(
+    sayHello: function() {
+    console.log(this);
+    console.log(arguments);
+    console.log("Hello World");
+         )   
+}
+
+
+ 
